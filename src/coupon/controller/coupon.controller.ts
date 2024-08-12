32e7de10e40fc2b064f7b35dcca17d2a19a244df
aiminghee3 <zzuni3423@naver.com>
2024-08-12 21:22:21 +0900

    add: http 테스트 용 코드 추가

--- src/coupon/controller/coupon.controller.ts
+++ src/coupon/controller/coupon.controller.ts
@@ -23,4 +23,9 @@ export class CouponController {
   async getLeftCouponList() : Promise<Coupon[]>{
     return await this.couponService.getCouponList();
   }
+
+  @Get('test')
+  async test(){
+    console.log('test');
+  }
 }
